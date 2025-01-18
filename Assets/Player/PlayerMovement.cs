9ce1db653a37d8d27eacb29dd0182e9a4bd25231
RobertBranchaud <rkbranchaud@wpi.edu>
2025-01-18 17:00:07 -0500

    Completed player movement
    blink + window

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -6,9 +6,6 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpHeight = 5f;
 
-    static int PLAYER_LAYER = 3;
-    static int PLATFORM_LAYER = 7;
-
     Rigidbody2D rb;
 
     public bool grounded;
@@ -18,14 +15,11 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundLayer;
     public LayerMask platformLayer;
     
-    private int platformLayerIndex;
-    private bool droppingThroughPlatform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        platformLayerIndex = LayerMask.NameToLayer("Platform");
     }
     public Vector2 v1 = new Vector2();
 
@@ -48,36 +42,8 @@ public class PlayerMovement : MonoBehaviour
             grounded = false;
         }
         
-        if (Input.GetKeyDown(KeyCode.S) && Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, platformLayer))
-        {
-            // StartCoroutine(DropThroughPlatform());
-            transform.position = (Vector2)transform.position + new Vector2(0f, -0.6f);
-        }
         
-    }
-
-    private IEnumerator DropThroughPlatform()
-    {
-        // Temporarily disable collision with the platform
-        Physics2D.IgnoreLayerCollision(PLAYER_LAYER, PLATFORM_LAYER, true);
-        Debug.Log("Start falling");
-
-
-        // Wait for a short time to ensure the player falls through
-        yield return new WaitForSeconds(0.3f);
-
-        // Re-enable collision with the platform
-        Physics2D.IgnoreLayerCollision(PLAYER_LAYER, PLATFORM_LAYER, false);
-        Debug.Log("Stop falling");
-
-    }
-    
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (droppingThroughPlatform && collision.gameObject.layer == PLATFORM_LAYER)
-        {
-            Physics2D.IgnoreLayerCollision(PLAYER_LAYER, PLATFORM_LAYER, true);
-        }
+        
     }
     
 }
