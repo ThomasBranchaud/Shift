4a4c813a305a9f6761187f28928c4c4bfc84b574
RobertBranchaud <rkbranchaud@wpi.edu>
2025-01-18 21:13:52 -0500

    MeleeBox

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -13,38 +13,68 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f; 
     public LayerMask groundLayer;
-    public LayerMask platformLayer;
+    public LayerMask defau;
     
+    public string facing;
+
+    public bool inMelee = true;
+
+    public GameObject meleeBox;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    public Vector2 v1 = new Vector2();
 
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer)
+                     || Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, defau);
+
 
         if(Input.GetKey(KeyCode.A)){
             rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+            facing = "left";
         }else if(Input.GetKey(KeyCode.D)){
             rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+            facing = "right";
         }else {
             rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
         }
 
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer) 
-                    || Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, platformLayer);
         if(Input.GetKeyDown(KeyCode.W) && grounded){
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
             grounded = false;
         }
         
-        
+        // attack
+        if(Input.GetMouseButtonDown(0)){
+            if(inMelee){
+                meleeAttack();
+            } else {
+                rangedAttack();
+            }
+        }
         
     }
+
+    void meleeAttack(){
+        Debug.Log("attacking");
+        Vector2 attackPos = (Vector2)transform.position + new Vector2(0.92f, 0f);
+        if(facing == "left"){
+            attackPos = (Vector2)transform.position + new Vector2(-0.92f, 0f);
+        }
+        GameObject atkbox = Instantiate(meleeBox, attackPos, Quaternion.identity);
+        Debug.Log(atkbox.name);
+        Destroy(atkbox, 0.15f);
+
+    }
+
+    
+
+    void rangedAttack(){}
     
 }
 
--- Assets/npcMovement.cs
+++ Assets/npcMovement.cs
@@ -17,7 +17,7 @@ public class npcMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        enemyHealth=5
+        enemyHealth=5;
         mask = LayerMask.GetMask("Player","Ground");
         Direction = "Right";
 
@@ -49,20 +49,20 @@ void Attack(GameObject Player )
     if((transform.position.x - Player.transform.position.x) > 1)
         {
             rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
-            Debug.Log("CHASING LEFT" + "Distance is" + (transform.position.x - Player.transform.position.x));
+            //Debug.Log("CHASING LEFT" + "Distance is" + (transform.position.x - Player.transform.position.x));
 
         }
     if( (Player.transform.position.x - transform.position.x) > 1)
         {
             rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
-            Debug.Log("CHASING RIGHT" + "Distance is" + (Player.transform.position.x - transform.position.x));
+            //Debug.Log("CHASING RIGHT" + "Distance is" + (Player.transform.position.x - transform.position.x));
             
             
         }
     else if(Vector2.Distance(transform.position,Player.transform.position)<1 && Vector2.Distance(transform.position,Player.transform.position)>-1)
     {
     
-        Debug.Log("ATTACKKKKKK");
+      //  Debug.Log("ATTACKKKKKK");
     }
     
     
@@ -89,14 +89,14 @@ void Patrol()
         {
             if (hit.collider.CompareTag("Player")) // Check if the first hit object is a player
             {
-                Debug.Log("HIT PLAYER");
+                //Debug.Log("HIT PLAYER");
                 Player = hit.collider.gameObject;
                 state = "Attack";
                 // Add logic for detecting the player here
             }
             else
             {
-                Debug.Log("First object hit is not a player, it is: " + hit.collider.gameObject.name);
+                //Debug.Log("First object hit is not a player, it is: " + hit.collider.gameObject.name);
             }
         }
         else
@@ -126,6 +126,14 @@ void Patrol()
         if(state == "Attack")
         {
             Attack(Player);
+        }
+    }
+
+
+    // GETTING HIT
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.name == "Melee Box(Clone)"){
+            Debug.Log("Enemy has been hit!");
         }
     }
 }
